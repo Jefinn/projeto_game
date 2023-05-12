cee755ecec97d21644f60c278c852cf4fe49f4c7
Jeferson Silva <jefersonsantossilva010@gmail.com>
2023-05-12 12:12:16 -0300

    12/05/2023 Finalização

--- forca.py
+++ forca.py
@@ -1,37 +1,253 @@
-# JOGO DA FORCA#
-def jogar():
-    print('\n*******************************')
-    print('Seja Bem-Vindo ao Jogo da Forca')
-    print('*******************************')
+import random
+def bem_vindo():
+    print("\n*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
 
-    palavra_secreta = 'banana'
-    ltr_certas = ['-' '-' '-' '-' '-' '-']
+def jogar():
+    bem_vindo()
+
+    print('\ntemas disponível para jogo'.upper())
+    print('(1)Frutas (2)Comida (3)Desenho'.upper())
+    escolha = int(input('\nQual tema você deseja jogar: ').upper())
+
+    if escolha == 1:
+        tema_frutas()
+
+    if escolha == 2:
+        tema_comida()
+
+    if escolha == 3:
+        tema_desenho()
+def tema_frutas():
+    with open('temas_forca/frutas.txt', 'r') as arquivo:
+        palavra = []
+
+        for linha in arquivo:
+            linha = linha.strip()
+            palavra.append(linha)
+
+    posicionamento = random.randrange(0, len(palavra))
+    palavra_secreta = palavra[posicionamento].upper()
+
+    letras_acertadas = ['_' for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
-    while (not enforcou and not acertou):
+    print(letras_acertadas)
 
-        chute = input('\nInforme uma letra: ')
-        chute = chute.upper()
-        chute = chute.strip()
+    while (not acertou and not enforcou):
 
-        index = 0
-        for letra in palavra_secreta:
-            letra = letra.upper()
+        chute = input("\nQual letra? ")
+        chute = chute.strip().upper()
 
-            if (chute == letra):
-                ltr_certas[index] = letra
-            index = index + 1
-
-            print(ltr_certas)
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
 
         else:
-            print('A letra informada não existe')
-            print('Tente novamente')
+            erros += 1
+            desenha_forca(erros)
 
-    print('Jogando....')
+        enforcou = erros == 7
+        acertou = '_' not in letras_acertadas
+        print(letras_acertadas)
 
+    if acertou:
+        print('\nVocê acertou a palavra secreta')
 
-if (__name__ == "__main__"):
+        print('\nDeseja jogar de novo'.upper())
+        loop = int(input(' (1) Sim  (2) Não '.upper().strip()))
+        if loop == 1:
+            jogar()
+        else:
+            print('Fim de Jogo'.upper())
+
+    if enforcou:
+        print('\nVocê errou a palavra secreta')
+        print('A palavra secreta é {}'.format(palavra_secreta))
+
+        print('\nDeseja jogar de novo'.upper())
+        loop = int(input(' (1) Sim  (2) Não '.upper().strip()))
+        if loop == 1:
+            jogar()
+        else:
+            print('Fim de Jogo'.upper())
+
+def tema_comida():
+    with open('temas_forca/comida.txt', 'r') as arquivo:
+        palavra = []
+        for linha in arquivo:
+            linha = linha.strip()
+            palavra.append(linha)
+
+    posicionamento = random.randrange(0, len(palavra))
+
+    palavra_secreta = palavra[posicionamento].upper()
+
+    letras_acertadas = ['_' for letra in palavra_secreta]
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+    while (not acertou and not enforcou):
+
+        chute = input("\nQual letra? ")
+        chute = chute.strip().upper()
+
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+
+        else:
+            erros += 1
+            desenha_forca(erros)
+
+        enforcou = erros == 7
+        acertou = '_' not in letras_acertadas
+        print(letras_acertadas)
+
+    if acertou:
+        print('\nVocê acertou a palavra secreta')
+
+        print('\nDeseja jogar de novo'.upper())
+        loop = int(input(' (1) Sim  (2) Não '.upper().strip()))
+        if loop == 1:
+            jogar()
+        else:
+            print('Fim de Jogo'.upper())
+
+    if enforcou:
+        print('\nVocê errou a palavra secreta')
+        print('A palavra secreta é {}'.format(palavra_secreta))
+
+        print('\nDeseja jogar de novo'.upper())
+        loop = int(input(' (1) Sim  (2) Não '.upper().strip()))
+        if loop == 1:
+            jogar()
+        else:
+            print('Fim de Jogo'.upper())
+
+def tema_desenho():
+    with open('temas_forca/desenho.txt', 'r') as arquivo:
+        palavra = []
+        for linha in arquivo:
+            linha = linha.strip()
+            palavra.append(linha)
+
+    posicionamento = random.randrange(0, len(palavra))
+
+    palavra_secreta = palavra[posicionamento].upper()
+
+    letras_acertadas = ['_' for letra in palavra_secreta]
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+    while (not acertou and not enforcou):
+
+        chute = input("\nQual letra? ")
+        chute = chute.strip().upper()
+
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+
+        else:
+            erros += 1
+            desenha_forca(erros)
+
+        enforcou = erros == 7
+        acertou = '_' not in letras_acertadas
+        print(letras_acertadas)
+
+    if acertou:
+        print('\nVocê acertou a palavra secreta')
+
+        print('\nDeseja jogar de novo'.upper())
+        loop = int(input(' (1) Sim  (2) Não '.upper().strip()))
+        if loop == 1:
+            jogar()
+        else:
+            print('Fim de Jogo'.upper())
+
+    if enforcou:
+        print('\nVocê errou a palavra secreta')
+        print('A palavra secreta é {}'.format(palavra_secreta))
+
+        print('\nDeseja jogar de novo'.upper())
+        loop = int(input(' (1) Sim  (2) Não '.upper().strip()))
+        if loop == 1:
+            jogar()
+        else:
+            print('Fim de Jogo'.upper())
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+if __name__ == '__main__':
     jogar()
--- painel_jogos.py
+++ painel_jogos.py
@@ -9,11 +9,11 @@ print('\n (1) ADIVINHA - (2) FORCA')
 
 jogo = int(input('Qual jogo você deseja jogar: '))
 
-if(jogo == 1):
+if jogo == 1:
     print('\nJogando Adivinha')
     adivinhacao.jogar()
 
-elif(jogo == 2):
+elif jogo == 2:
     print('\nJogando Forca')
     forca.jogar()
 
